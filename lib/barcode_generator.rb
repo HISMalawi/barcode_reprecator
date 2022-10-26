9477c60a9f3d1f9ff3a5724df8a08612b8978b6c
hopgausi <hopgausi20@gmail.com>
2022-10-26 12:15:21 +0200

    Refactored generator service

--- lib/barcode_generator.rb
+++ lib/barcode_generator.rb
@@ -2,7 +2,6 @@ require 'auto12epl'
 module BarcodeGeneratorService
   def self.generate_specimen_label(accession_number)
     settings = YAML.load_file("#{Rails.root}/config/application.yml")
-    # accession_number = 'KCH2200500042'
     specimen = Specimen.find_by(accession_number: accession_number)
     if specimen.nil?
       return nil
@@ -40,35 +39,10 @@ module BarcodeGeneratorService
     numerical_acc_num = numerical_ac(specimen.accession_number)
 
     auto = Auto12Epl.new
-    s =  auto.generate_epl(last_name.to_s, first_name.to_s, middle_initial.to_s, npid.to_s, dob, age.to_s,
-                           gender.to_s, col_datetime, col_by.to_s, tname.to_s,
-                           stat_el, formatted_acc_num.to_s, numerical_acc_num)
-
-    # if !history.blank?
-      s += "\n##########BEGIN FORM##########\n\n"
-      s += "\nN\nq616\nQ090,0\nZT\n"
-      s += "A150,50,0,3,1,1,N,\"#{settings['facility_name']}\"\n"
-      s += "A80,90,0,3,1,1,N,\"Laboratory Test Order Form V2.0.0\"\n"
-      s += "A30,130,0,3,1,1,N,\"Patient : #{patient.name} (#{gender})\"\n"
-      s += "A30,170,0,3,1,1,N,\"Patient ID : #{npid}\"\n"
-      s += "A30,210,0,3,1,1,N,\"Patient DOB: #{dob}\"\n"
-      s += "A30,250,0,3,1,1,N,\"Ordered By : #{tests.first.requested_by}\"\n"
-      s += "A30,290,0,3,1,1,N,\"Ordered From : #{tests.first.visit.ward_or_location}\"\n"
-      s += "A30,330,0,3,1,1,N,\"Specimen Type : #{specimen.specimen_type.name}\"\n"
-      s += "A30,370,0,3,1,1,N,\"Priority : #{specimen.priority}\"\n"
-      s += "A30,410,0,3,1,1,N,\"Collected at : #{specimen.date_of_collection.strftime("%d %b, %Y %H:%M") if !specimen.date_of_collection.nil?}\"\n"
-      # s += "A30,450,0,3,1,1,N,\"Clinical History : #{history}\"\n"
-      s += "A30,490,0,3,1,1,N,\"Tests\"\n"
-      line = 530
-      test_names.each do |name|
-        s += "A50,#{line},0,3,1,1,N,\"-#{name}\"\n"
-        line +=40
-      end
-      s += "B180,#{line},0,1A,2,2,120,N,\"#{numerical_acc_num}\"\n"
-      s += "P1\n\n"
-    # end
-
-    return s
+    auto.generate_epl(last_name.to_s, first_name.to_s, middle_initial.to_s, npid.to_s, dob, age.to_s,
+                      gender.to_s, col_datetime, col_by.to_s, tname.to_s,
+                      stat_el, formatted_acc_num.to_s, numerical_acc_num
+                    )
   end
 
   def self.format_ac(num)

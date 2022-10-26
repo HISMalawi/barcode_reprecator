6ca42175055b42454ea9bdf1dc7b4c3cb8d970e1
hopgausi <hopgausi20@gmail.com>
2022-10-26 12:11:27 +0200

    Added checker for availability of order per given accession number

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   def print_and_redirect(print_url, redirect_url, message = "Printing, please wait...")
+    accession_number = params[:accession_number].include?('$') ? params[:accession_number].delete_suffix('$') : params[:accession_number]
+    specimen = Specimen.find_by(accession_number: accession_number)
+    if specimen.nil?
+      flash[:alert] = "Order for accession number: #{accession_number} not found"
+      redirect_to '/'
+      return
+    end
     @print_url = print_url
     @redirect_url = redirect_url
     @message = message
